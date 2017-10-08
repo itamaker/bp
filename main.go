8c02cafed3af3737869a3e006ecf893c9aba50dd
Ke Yang <ofveravi@gmail.com>
2017-10-08 18:51:26 +0800

    now able to insert new alias into db

--- main.go
+++ main.go
@@ -7,11 +7,14 @@ import (
 	"database/sql"
 	"fmt"
 	// "os/exec"
-	"strconv"
+	// "strconv"
+	"bufio"
+	"strings"
 
 	_ "github.com/mattn/go-sqlite3"
 )
 
+// prepare and return sqlite3 database
 func prepareDB() *sql.DB {
 	db, err1 := sql.Open("sqlite3", "./betaphor.sqlite3")
 	if err1 != nil {
@@ -31,6 +34,49 @@ func prepareDB() *sql.DB {
 	return db
 }
 
+// prompt for new alias input
+func promptNewAlias() {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Enter alias name: ")
+	alias, err1 := reader.ReadString('\n')
+	if err1 != nil {
+		log.Fatal(err1.Error())
+		os.Exit(1)
+	}
+	alias = strings.TrimSpace(alias)
+
+	fmt.Print("Enter command literal: ")
+	command, err2 := reader.ReadString('\n')
+	if err2 != nil {
+		log.Fatal(err2.Error())
+		os.Exit(2)
+	}
+	command = strings.TrimSpace(command)
+
+	insertNewAlias(alias, command)
+}
+
+func insertNewAlias(alias string, command string) {
+	db := prepareDB()
+	insert, err1 := db.Prepare("INSERT INTO `aliases` (`alias`, `command`) VALUES (?, ?)")
+	if err1 != nil {
+		log.Fatal(err1.Error())
+		os.Exit(1)
+	}
+	_, err2 := insert.Exec(alias, command)
+	if err2 != nil {
+		log.Fatal(err2.Error())
+		os.Exit(2)
+	}
+	rows, _ := db.Query("SELECT `alias`, `command` FROM `aliases`")
+	var insertedAlias string
+	var insertedCmd string
+	for rows.Next() {
+		rows.Scan(&insertedAlias, &insertedCmd)
+		fmt.Println(insertedAlias + ": " + insertedCmd)
+	}
+}
+
 func init() {
 	prepareDB()
 }
@@ -47,17 +93,21 @@ func init() {
 // }
 
 func main() {
-	database, _ := sql.Open("sqlite3", "./nraboy.db")
-	statement1, _ := database.Prepare("CREATE TABLE IF NOT EXISTS people (id INTEGER PRIMARY KEY, firstname TEXT, lastname TEXT)")
-	statement1.Exec()
-	statement2, _ := database.Prepare("INSERT INTO people (firstname, lastname) VALUES (?, ?)")
-	statement2.Exec("Nic", "Raboy")
-	rows, _ := database.Query("SELECT id, firstname, lastname FROM people")
-	var id int
-	var firstname string
-	var lastname string
-	for rows.Next() {
-		rows.Scan(&id, &firstname, &lastname)
-		fmt.Println(strconv.Itoa(id) + ": " + firstname + " " + lastname)
+	operation := os.Args[1]
+	if operation == "add" {
+		promptNewAlias()
 	}
+	// database, _ := sql.Open("sqlite3", "./nraboy.db")
+	// statement1, _ := database.Prepare("CREATE TABLE IF NOT EXISTS people (id INTEGER PRIMARY KEY, firstname TEXT, lastname TEXT)")
+	// statement1.Exec()
+	// statement2, _ := database.Prepare("INSERT INTO people (firstname, lastname) VALUES (?, ?)")
+	// statement2.Exec("Nic", "Raboy")
+	// rows, _ := database.Query("SELECT id, firstname, lastname FROM people")
+	// var id int
+	// var firstname string
+	// var lastname string
+	// for rows.Next() {
+	// 	rows.Scan(&id, &firstname, &lastname)
+	// 	fmt.Println(strconv.Itoa(id) + ": " + firstname + " " + lastname)
+	// }
 }
